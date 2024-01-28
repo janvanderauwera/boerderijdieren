0fed636d7417406f5b3b01cdc0ff926bcd78b2d1
janvanderauwera <149608991+janvanderauwera@users.noreply.github.com>
2024-01-28 19:43:26 +0100

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,49 +1,57 @@
 const dieren = [{
   naam : "Haan", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/hahn_kikeriki.mp3",
-  image : "Boerderij/dieren/haan.png", 
+  image : "Boerderij/dieren/haan.png",
+  naamAudio : "Boerderij/dieren/namen/haan.mp3" 
 },
 
 {
   naam : "Kip", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/huehner.mp3",
   image : "Boerderij/dieren/kip.png", 
+  naamAudio : "Boerderij/dieren/namen/kip.mp3" 
 
 },
 {
   naam : "Koe", 
   audio : "https://www.fesliyanstudios.com/play-mp3/6520",
   image : "Boerderij/dieren/koe.png", 
+  naamAudio : "Boerderij/dieren/namen/koe.mp3" 
 
 },
 {
   naam : "Kuiken", 
   audio : "Boerderij/dieren/Audio/chick.mp3",
   image : "Boerderij/dieren/kuiken.png", 
+  naamAudio : "Boerderij/dieren/namen/kuiken.mp3" 
 
 },
 {
   naam : "Lam", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/schaf.mp3",
   image : "Boerderij/dieren/lam.png", 
+  naamAudio : "Boerderij/dieren/namen/lam.mp3" 
 
 },
 {
   naam : "Paard", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/pferd_whinnert.mp3",
   image : "Boerderij/dieren/paard.png", 
+  naamAudio : "Boerderij/dieren/namen/paard.mp3" 
 
 },
 {
   naam : "Schaap", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/schafe.mp3",
-  image : "Boerderij/dieren/schaap.png", 
+  image : "Boerderij/dieren/schaap.png",
+  naamAudio : "Boerderij/dieren/namen/schaap.mp3"  
 
 },
 {
   naam : "Stier", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/ochse.mp3",
   image : "Boerderij/dieren/stier.png", 
+  naamAudio : "Boerderij/dieren/namen/stier.mp3" 
 
 },
 
@@ -51,6 +59,7 @@ const dieren = [{
   naam : "Kat", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/Katze_miaut.mp3",
   image : "Boerderij/dieren/kat.png", 
+  naamAudio : "Boerderij/dieren/namen/kat.mp3" 
 
 },
 
@@ -58,6 +67,7 @@ const dieren = [{
   naam : "Hond", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/Bluthund_jault.mp3",
   image : "Boerderij/dieren/hond.png", 
+  naamAudio : "Boerderij/dieren/namen/hond.mp3" 
 
 },
 
@@ -92,7 +102,8 @@ const dieren = [{
 {
   naam : "Eend", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/Ente_quackt.mp3",
-  image : "Boerderij/dieren/eend.png", 
+  image : "Boerderij/dieren/eend.png",
+  naamAudio : "Boerderij/dieren/namen/eend.mp3" 
 
 },
 /*
@@ -107,6 +118,7 @@ const dieren = [{
   naam : "Varken", 
   audio : "https://freeanimalsounds.org/wp-content/uploads/2017/07/schwein.mp3",
   image : "Boerderij/dieren/varken.png", 
+  naamAudio : "Boerderij/dieren/namen/varken.mp3" 
 
 },
 
@@ -135,6 +147,9 @@ document.querySelector(".wrapper").innerHTML = html;
 
 let dierenFotos = document.querySelectorAll(".boerderijDier");
 let dierenGeluid = document.querySelector(".dierGeluid");
+let dierNaamAudio = document.querySelector(".dierNaamAudio");
+let boerderijDierNamen = document.querySelectorAll(".boerderijDierNaam");
+
 dierenFotos.forEach((foto)=>{
   foto.addEventListener ("click", function(){
    
@@ -158,6 +173,30 @@ dierenGeluid.play();
 
   
 });
+
+boerderijDierNamen.forEach((naam)=>{
+
+  naam.addEventListener("click", ()=>{
+
+    let dierNaam = naam.innerHTML;
+    dieren.forEach((dier)=>{
+      if (dier.naam === dierNaam){
+        matchingDier = dier;
+      }
+    });
+if (matchingDier){
+  dierNaamAudio.src = matchingDier.naamAudio;
+}
+
+
+dierNaamAudio.play();
+    
+    
+    });
+    
+
+});
+
 
 
 
